db64287efd4e4f32d31552a18b2709651d44eab0
Sebastian Kropp <sebastian.kropp@outlook.com>
2015-12-23 15:25:20 +0100

    removed uneccessary definitions

--- app/src/tsd.missing.d.ts
+++ app/src/tsd.missing.d.ts
@@ -1,20 +1,11 @@
-// missing 
+// missing definitions
 declare module d3 {
-    export function tip(): any;
-	export function contextMenu(any): any;
-	export interface Base {
-		timeline: any;
-		contextMenu: any;
-		barChart: any;
-	}
+    function tip(): any;
+	function contextMenu(any): any;
 }
 
 declare module moment {
     interface Duration {
         format(format: string): string;
-        format(): string;
     }
 }
-
-// d3 tip
-declare var define, tip;
